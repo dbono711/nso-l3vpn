43a9291daa24d2eb5c7223290fbe72fe84f90b13
Darren Bono <dbono215@gmail.com>
2026-01-05 21:07:21 -0500

    fix: Bump l3vpn package version to 1.1.1 and reorder premod callback code

--- l3vpn/python/l3vpn/main.py
+++ l3vpn/python/l3vpn/main.py
@@ -17,13 +17,12 @@ class ServiceCallbacks(Service):
     @Service.pre_modification
     def cb_pre_modification(self, tctx, op, kp, root, proplist):
         """Docstring Missing."""
-        self.log.info(f"Service premod(service={kp})")
-        service = cd(root, kp)
-        ctx = ServiceContext(log=self.log, root=root, service=service)
-
         if op == 2:
             return  # nothing to do for 'Delete(2)' operation
 
+        self.log.info(f"Service premod(service={kp})")
+        service = cd(root, kp)
+        ctx = ServiceContext(log=self.log, root=root, service=service)
         PreMod(ctx).fill()
 
     @Service.create
